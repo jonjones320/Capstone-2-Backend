abc988785525ec5e08405db6fabc5663db1e6ba9
JJ <jonjones320@gmail.com>
2024-08-30 04:16:07 -0700

    Changed how much of the Db URI was expected

--- config.test.js
+++ config.test.js
@@ -18,10 +18,10 @@ describe("config can come from env", function () {
     delete process.env.BCRYPT_WORK_FACTOR;
     delete process.env.DATABASE_URL;
 
-    expect(config.getDatabaseUri()).toEqual("ranner");
+    expect(config.getDatabaseUri()).toEqual("postgresql://ranner");
     process.env.NODE_ENV = "test";
 
-    expect(config.getDatabaseUri()).toEqual("ranner_test");
+    expect(config.getDatabaseUri()).toEqual("postgresql://ranner_test");
   });
 })
 
