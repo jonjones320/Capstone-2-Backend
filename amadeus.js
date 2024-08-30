962776157e84cd30b5b4d971b912afa17e9c3545
JJ <jonjones320@gmail.com>
2024-08-30 06:05:58 -0700

    Removed debugging logs

--- amadeus.js
+++ amadeus.js
@@ -3,10 +3,6 @@
 require("dotenv").config();
 const Amadeus = require("amadeus");
 
-console.log("AMADEUS_CLIENT_ID:", process.env.AMADEUS_CLIENT_ID);
-console.log("AMADEUS_CLIENT_SECRET:", process.env.AMADEUS_CLIENT_SECRET);
-
-
 const amadeus = new Amadeus({
   clientId: process.env.AMADEUS_CLIENT_ID,
   clientSecret: process.env.AMADEUS_CLIENT_SECRET,
@@ -14,6 +10,5 @@ const amadeus = new Amadeus({
   hostname: process.env.NODE_ENV === "production" ? "production" : undefined,
 });
 
-console.log(amadeus.client);
 
 module.exports = amadeus;
