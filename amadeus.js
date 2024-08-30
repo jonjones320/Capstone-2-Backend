3d302f358b3eb220446823eb1a91d03aa4b8a4b2
JJ <jonjones320@gmail.com>
2024-08-30 05:49:08 -0700

    Debugging: adjusted environment variables and instance of Amadeus.

--- amadeus.js
+++ amadeus.js
@@ -1,14 +1,19 @@
 // Amadeus API SDK initialization
 
-const Amadeus = require("amadeus");
 require("dotenv").config();
+const Amadeus = require("amadeus");
+
+console.log("AMADEUS_CLIENT_ID:", process.env.AMADEUS_CLIENT_ID);
+console.log("AMADEUS_CLIENT_SECRET:", process.env.AMADEUS_CLIENT_SECRET);
 
 
 const amadeus = new Amadeus({
   clientId: process.env.AMADEUS_CLIENT_ID,
   clientSecret: process.env.AMADEUS_CLIENT_SECRET,
-  logLevel: 'debug',
+  logLevel: "debug",
   hostname: process.env.NODE_ENV === "production" ? "production" : undefined,
 });
+
+console.log(amadeus.client);
 
 module.exports = amadeus;
