3b148e6710995c3093fb1825124d866e955cc659
JJ <jonjones320@gmail.com>
2024-08-30 19:33:53 -0700

    Reformatted the models common tests to fit the current model setup.

--- models/_testCommon.js
+++ models/_testCommon.js
@@ -1,49 +1,51 @@
 const bcrypt = require("bcrypt");
-
 const db = require("../db.js");
 const { BCRYPT_WORK_FACTOR } = require("../config");
 
-const testJobIds = [];
 
 async function commonBeforeAll() {
   // noinspection SqlWithoutWhere
-  await db.query("DELETE FROM companies");
-  // noinspection SqlWithoutWhere
   await db.query("DELETE FROM users");
+  // noinspection SqlWithoutWhere
+  await db.query("DELETE FROM trips");
+  // noinspection SqlWithoutWhere
+  await db.query("DELETE FROM flights");
 
   await db.query(`
-    INSERT INTO companies(handle, name, num_employees, description, logo_url)
-    VALUES ('c1', 'C1', 1, 'Desc1', 'http://c1.img'),
-           ('c2', 'C2', 2, 'Desc2', 'http://c2.img'),
-           ('c3', 'C3', 3, 'Desc3', 'http://c3.img')`);
+    INSERT INTO trips(trip_id, 
+                      name, 
+                      username, 
+                      location_id, 
+                      start_date, 
+                      end_date, 
+                      budget)
+    VALUES ('t1', 'Trip1', 'u1', 'l1', '2022-01-01', '2022-01-10', 1000),
+           ('t2', 'Trip2', 'u1', 'l2', '2022-02-01', '2022-02-10', 2000),
+           ('t3', 'Trip3', 'u2', 'l3', '2022-03-01', '2022-03-10', 3000)`);
 
-  const resultsJobs = await db.query(`
-    INSERT INTO jobs (title, salary, equity, company_handle)
-    VALUES ('Job1', 100, '0.1', 'c1'),
-           ('Job2', 200, '0.2', 'c1'),
-           ('Job3', 300, '0', 'c1'),
-           ('Job4', NULL, NULL, 'c1')
-    RETURNING id`);
-  testJobIds.splice(0, 0, ...resultsJobs.rows.map(r => r.id));
+  await db.query(`
+    INSERT INTO flights (flight_number, trip_id, origin, destination)
+    VALUES ('A123', 't1', 'SFO', 'JFK'), 
+           ('B456', 't1', 'RDU', 'SEA'),
+           ('C789', 't2', 'DEN', 'LAX')
+    RETURNING flight_number`);
 
   await db.query(`
         INSERT INTO users(username,
                           password,
                           first_name,
                           last_name,
-                          email)
-        VALUES ('u1', $1, 'U1F', 'U1L', 'u1@email.com'),
-               ('u2', $2, 'U2F', 'U2L', 'u2@email.com')
+                          email,
+                          is_admin)
+        VALUES ('u1', $1, 'U1F', 'U1L', 'u1@email.com', false),
+               ('u2', $2, 'U2F', 'U2L', 'u2@email.com', false),
+               ('a1', $3, 'A1F', 'A1L', 'a1@email.com', true)
         RETURNING username`,
       [
         await bcrypt.hash("password1", BCRYPT_WORK_FACTOR),
         await bcrypt.hash("password2", BCRYPT_WORK_FACTOR),
+        await bcrypt.hash("password3", BCRYPT_WORK_FACTOR),
       ]);
-
-  await db.query(`
-        INSERT INTO applications(username, job_id)
-        VALUES ('u1', $1)`,
-      [testJobIds[0]]);
 }
 
 async function commonBeforeEach() {
@@ -63,6 +65,5 @@ module.exports = {
   commonBeforeAll,
   commonBeforeEach,
   commonAfterEach,
-  commonAfterAll,
-  testJobIds,
+  commonAfterAll
 };
