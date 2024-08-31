5442ac3fa9026b94557f2bfef52288d02967bdd0
JJ <jonjones320@gmail.com>
2024-08-31 15:36:04 -0700

    Removed seeding for Id's that are serialized upon row creation in the database.

--- ranner-seed.sql
+++ ranner-seed.sql
@@ -17,10 +17,10 @@ VALUES ('testuser',
         TRUE);
 
 
-INSERT INTO trips (trip_id, name, username, location_id, start_date, end_date, budget)
-VALUES (1, 'New York Trip', 'testuser', 'NYC', '2025-01-01', '2025-01-10', 1000.00),
-       (2, 'Los Angeles Trip', 'testuser', 'LAX', '2025-02-01', '2025-02-05', 1500.00),
-       (3, 'San Francisco Trip', 'testadmin', 'SFO', '2025-03-01', '2025-03-07', 2000.00);
+INSERT INTO trips (name, username, location_id, start_date, end_date, budget)
+VALUES ('New York Trip', 'testuser', 'NYC', '2025-01-01', '2025-01-10', 1000.00),
+       ('Los Angeles Trip', 'testuser', 'LAX', '2025-02-01', '2025-02-05', 1500.00),
+       ('San Francisco Trip', 'testadmin', 'SFO', '2025-03-01', '2025-03-07', 2000.00);
 
 
 INSERT INTO flights (flight_number, trip_id, origin, destination)
@@ -28,7 +28,7 @@ VALUES ('AA123', 1, 'JFK', 'LAX'),
        ('AA124', 2, 'LAX', 'JFK'),
        ('DL456', 3, 'LAX', 'SFO');
 
-INSERT INTO accommodations (accommodation_id, trip_id, name, check_in, check_out)
-VALUES (1, 1, 'Hotel 1', '2025-01-01', '2025-01-10'),
-       (2, 2, 'Hotel 2', '2025-02-01', '2025-02-05'),
-       (3, 3, 'Hotel 3', '2025-03-01', '2025-03-07');
+INSERT INTO accommodations (trip_id, name, check_in, check_out)
+VALUES (1, 'Hotel 1', '2025-01-01', '2025-01-10'),
+       (2, 'Hotel 2', '2025-02-01', '2025-02-05'),
+       (3, 'Hotel 3', '2025-03-01', '2025-03-07');
