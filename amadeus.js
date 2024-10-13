065cac4f7cbe3887dd98e7d8f9db9a621bbf29ee
JJ <jonjones320@gmail.com>
2024-10-12 22:04:59 -0700

    Hardcoded 'hostname' to be 'test' because the current Amadeus key and secret are for Amadeus development environment, not the paid production environment.

--- amadeus.js
+++ amadeus.js
@@ -6,8 +6,7 @@ const Amadeus = require("amadeus");
 const amadeus = new Amadeus({
   clientId: process.env.AMADEUS_CLIENT_ID,
   clientSecret: process.env.AMADEUS_CLIENT_SECRET,
-  // logLevel: "debug",
-  hostname: process.env.NODE_ENV === "production" ? "production" : undefined,
+  hostname: 'test' // Current Amadeus ID and SECRET are for test environment only.
 });
 
 
