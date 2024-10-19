52803dfc42357e13beaa792c9f0930ee3e7b20da
JJ <jonjones320@gmail.com>
2024-10-19 03:35:06 -0700

    Modified to handle ISO formatting from the SQL database formatting.

--- helpers/date.js
+++ helpers/date.js
@@ -1,4 +1,4 @@
-const { format, parse, isValid } = require('date-fns');
+const { format, parseISO } = require('date-fns');
 
 class InvalidDateError extends Error {
   constructor(message) {
@@ -12,25 +12,39 @@ const formatDate = (dateString) => {
     throw new InvalidDateError('Input must be a string');
   }
 
-  const formats = [
-    'yyyy-MM-dd',
-    'MM/dd/yyyy',
-    'dd/MM/yyyy',
-    'MM-dd-yyyy',
-    'dd-MM-yyyy',
-    'yyyy/MM/dd'
-  ];
-
   let date;
 
-  for (let fmt of formats) {
-    date = parse(dateString, fmt, new Date());
-    if (isValid(date)) {
-      return format(date, 'yyyy-MM-dd');
+  // Try to parse the date as an ISO string.
+  try {
+    date = parseISO(dateString);
+    if (isNaN(date.getTime())) {
+      throw new Error('Invalid ISO date');
+    }
+  } catch (error) {
+    // If ISO parsing fails, try other common formats
+    const formats = [
+      'yyyy-MM-dd',
+      'MM/dd/yyyy',
+      'dd/MM/yyyy',
+      'MM-dd-yyyy',
+      'dd-MM-yyyy',
+      'yyyy/MM/dd'
+    ];
+
+    for (let fmt of formats) {
+      date = parse(dateString, fmt, new Date());
+      if (!isNaN(date.getTime())) {
+        break;
+      }
+    }
+
+    if (isNaN(date.getTime())) {
+      throw new InvalidDateError(`Invalid date format: ${dateString}`);
     }
   }
 
-  throw new InvalidDateError(`Invalid date format: ${dateString}`);
+  // Format the date as YYYY-MM-DD
+  return format(date, 'yyyy-MM-dd');
 };
 
 module.exports = formatDate;
