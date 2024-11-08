90101dda1fb86c744b543990ba8522a0b40ebd91
JJ <jonjones320@gmail.com>
2024-11-08 04:01:56 -0800

    Updated CORS with specific HTTP methods and specifically allowed headers.

--- app.js
+++ app.js
@@ -19,8 +19,10 @@ const morgan = require("morgan");
 const app = express();
 
 app.use(cors({
-  origin: process.env.CORS_ORIGIN || 'http://localhost:3000',
-  credentials: true
+  origin: [process.env.CORS_ORIGIN, 'http://localhost:3000'],
+  credentials: true,
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
+  allowedHeaders: ['Content-Type', 'Authorization']
 }));
 app.use(express.json());
 app.use(morgan("tiny"));
